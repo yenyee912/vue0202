0b591c5236425ab7dd94ef7c9753e228eaa84ef0
yenyee912 <fuyazi1997@gmail.com>
2021-06-03 17:58:20 +0800

    lesson02-1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-table :items="allStudent" striped hover></b-table>
+    </b-container>
+
+    <b-container>
+      <b-table :items="allCourse" striped hover></b-table>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+
+  data(){
+    return{
+      allStudent: [],
+      msg: '',
+
+      allCourse: [],
+      msg2: '',
+    }
+  },
+
+  methods:{
+    async callAllAPIs(){
+      await Promise.all([
+        this.getAllStudents(),this.getAllCourses()
+      ])
+    },
+
+    async getAllStudents(){
+      try{
+        const x = await axios.get('http://sun.beyond.photos:5000/student')
+
+        if(x.data.data.length>0){
+          this.allStudent= x.data.data
+        }
+
+        else{
+          this.msg= x.data.msg
+        }
+      }
+
+      catch(err){
+        console.log(err.message)
+      }
+    },
+
+    async getAllCourses(){
+      try{
+        const x = await axios.get('http://localhost:5000/courses')
+
+        if(x.data.length>0){
+          this.allCourse= x.data
+        }
+
+        else{
+          this.msg2= x.data.msg
+        }
+      }
+
+      catch(err){
+        console.log(err.message)
+      }
+    }
+  },
+
+  mounted(){
+    this.callAllAPIs()
   }
 }
 </script>
